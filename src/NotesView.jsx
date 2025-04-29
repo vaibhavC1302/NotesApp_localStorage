0f26e1c7ba2daa1591399f660863f2b654ecb646
vaibhavC1302 <chauhanvaibhav13@gmail.com>
2025-04-29 18:40:26 +0530

    ain

--- src/NotesView.jsx
+++ src/NotesView.jsx
@@ -4,6 +4,7 @@ import NoteCard from './NoteCard'
 const NotesView = ({ notesData, setNotesData }) => {
     return (
         <div className='notesContainer'>
+            {notesData.length === 0 && <h2 className='noNoteText'>no notes</h2>}
             {notesData.map(note => {
                 return <NoteCard note={note} key={note.id} setNotesData={setNotesData} />
             })}
